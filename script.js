f9150be657c83c848b13048f0be3413ef4511ca6
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-22 17:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let inputValue = document.querySelectorAll(".code")
+for(let i=0;i<inputValue.length;i++){
+	if(i+1 < input.length && inputValue[i].value.length == 1){
+		inputValue[i+1].focus()
+	}
+}
