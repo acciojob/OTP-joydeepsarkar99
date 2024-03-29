e9b4e677d7a0ef4006436d96b95dbf74b29cc3cd
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-29 19:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //your JS code here. If required.
-let inputValue = document.querySelectorAll(".code")
-for(let i=0;i<inputValue.length;i++){
-	if(i+1 < input.length && inputValue[i].value.length == 1){
-		inputValue[i+1].focus()
-	}
+let inputs = document.querySelectorAll(".code")
+
+let index = 0
+inputs[0].focus()
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', (eventDetails) => {
+        let inputElement = eventDetails.target.value
+        if (inputElement.length == 1) {
+            if (index < inputs.length - 1) {
+                index++;
+                inputs[index].focus();
+            }
+        } else if (inputElement.length == 0) {
+            if (index > 0) {
+                index--;
+                inputs[index].focus();
+            }
+        }
+    });
 }
